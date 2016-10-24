64ce644c24189141aa62e3902a994758722f1c05
Pengsreang Srun <srunpengsreang@yahoo.com>
2016-10-25 00:00:32 +0700

    initial project

---
+++ ProjectTemplate/ProjectTemplate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectTemplate
+//
+//  Created by ស្រ៊ុន ប៉េងស្រ៊ាង on 10/24/16.
+//  Copyright © 2016 Srun Pengsreang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
